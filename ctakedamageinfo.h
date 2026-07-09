5990e3ad797c2f61a7f67e5ba203383d070cc146
Cruze03 <sachinfnatic@gmail.com>
2026-07-09 23:16:21 +0530

    update: ctakedamageinfo

--- ctakedamageinfo.h
+++ ctakedamageinfo.h
@@ -81,6 +81,7 @@ public:
 	int32_t m_nHealthBefore;
 	float m_flDamageDealt;
 	float m_flPreModifiedDamage;
+	VectorWS m_vDamagePosition;
 	int32_t m_nTotalledHealthLost;
 	float m_flTotalledDamageDealt;
 	float m_flTotalledPreModifiedDamage;
@@ -119,4 +120,4 @@ public:
 	{
 	}
 };
-static_assert(sizeof(CTakeDamageResult) == 80);
+static_assert(sizeof(CTakeDamageResult) == 96);
